af7c808e1d9246cfc5d8c35960990b0d415f3899
pghandour <pghandour@gmail.com>
2018-09-21 10:11:30 -0400

    Add files via upload

---
+++ client/src/components/AddCategory.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class AddCategory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      shortcode: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { name, description, shortcode } = this.state;
+
+    axios.post('/uploading/add-category', { name, description, shortcode })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    const { name, description, shortcode } = this.state;
+    return (
+        <div>
+            Add Category: <br/>
+            <form onSubmit={this.onSubmit}>
+            Name: <input type="text" name="name" value={name} onChange={this.onChange}/> <br/>
+            Description: <input type="text" name="description" value={description} onChange={this.onChange}/> <br/> 
+            Shortcode: <input type="text" name="shortcode" value={shortcode} onChange={this.onChange}/> <br/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+    );
+  }
+}
+
+export default AddCategory;  
